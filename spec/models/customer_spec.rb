699c9fac60327f6f6ad80b72bfc270bb6052950f
Michael Koulouvaris <mikekoul@gmail.com>
2022-08-01 18:20:32 -0600

    Add Customer model method for price

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Customer, type: :model do
     it { should have_many :customer_items }
     it { should have_many(:items).through(:customer_items) }
   end
+
+  describe '#methods' do
+    it 'calculates total price of items and coverts to dollars' do
+      ideal = Supermarket.create!(name: 'Ideal Market', location: 'Capitol Hill')
+      mike = Customer.create!(name: "Mike", supermarket_id: ideal.id)
+      banana = Item.create!(name: 'Banana', price: 200)
+      bread = Item.create!(name: 'Bread', price: 600)
+      CustomerItem.create!(customer_id: mike.id, item_id: banana.id)
+      CustomerItem.create!(customer_id: mike.id, item_id: bread.id)
+
+      expect(mike.total_price).to eq(800)
+    end
+  end
 end

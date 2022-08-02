86151f572d29bd2abd811796bc6d7443338067c5
Michael Koulouvaris <mikekoul@gmail.com>
2022-08-01 18:42:01 -0600

    Add link to supermarket items index on the supermarket show page

---
+++ spec/features/supermarkets/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'supermarket show page' do
+  describe '#show' do
+    it 'displays the name of the supermarket' do
+      ideal = Supermarket.create!(name: 'Ideal Market', location: 'Capitol Hill')
+      soopers = Supermarket.create!(name: 'King Soopers', location: 'Aurora')
+
+      visit "/supermarkets/#{ideal.id}"
+
+      expect(page).to have_content("Ideal Market")
+      expect(page).to_not have_content("King Soopers")
+    end
+
+    it 'has a link to view all the items belonging to the supermarket' do
+      ideal = Supermarket.create!(name: 'Ideal Market', location: 'Capitol Hill')
+      soopers = Supermarket.create!(name: 'King Soopers', location: 'Aurora')
+
+      visit "/supermarkets/#{ideal.id}"
+
+      expect(page).to have_link("All Items")
+      expect(page).to_not have_content("King Soopers")
+    end
+  end
+end
